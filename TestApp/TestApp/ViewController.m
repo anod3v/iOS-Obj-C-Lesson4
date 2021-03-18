105d30ad19ddae05683aa93d0210142e829f431f
Anoshkin <a.anoshkin@profsoft.pro>
2021-03-18 23:23:15 +0300

    project is set up

---
+++ TestApp/TestApp/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  TestApp
+//
+//  Created by Andrey on 08/03/2021.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self.view setBackgroundColor:[UIColor redColor]]; 
+}
+
+
+@end
