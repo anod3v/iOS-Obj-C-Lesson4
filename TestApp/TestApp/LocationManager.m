026687db50f84f328f7f8fc5a36e791870ba8840
Anoshkin <a.anoshkin@profsoft.pro>
2021-03-18 23:39:51 +0300

    done

---
+++ TestApp/TestApp/LocationManager.m
@@ -0,0 +1,61 @@
+//
+//  LocationManager.m
+//  TestApp
+//
+//  Created by Andrey on 18/03/2021.
+//
+
+#import "LocationManager.h"
+#import <CoreLocation/CoreLocation.h>
+
+typedef void(^LocationCallback)(CLLocation *location);
+
+@interface LocationManager() <CLLocationManagerDelegate>
+
+@property (nonatomic, strong) CLLocationManager *manager;
+//@property (nonatomic, copy) LocationCallback locationCallback;
+
+@end
+
+@implementation LocationManager
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        
+        self.manager = [CLLocationManager new];
+        self.manager.delegate = self;
+        self.manager.desiredAccuracy = kCLLocationAccuracyKilometer;
+        self.manager.distanceFilter = 500;
+        
+        [self.manager requestWhenInUseAuthorization];
+        
+    }
+    return self;
+}
+
+#pragma mark - Public
+
+- (void)start {
+    [self.manager startUpdatingLocation];
+}
+
+- (void)stop {
+    [self.manager stopUpdatingLocation];
+}
+
+#pragma mark - CLLocationManagerDelegate
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations {
+    
+    CLLocation *location = [locations firstObject];
+    if (location) {
+        NSLog(@"Геолокация пользователя \n\n %@", location);
+//        self.currentLocation = location;
+        
+        self.locationCallback(location);
+    }
+}
+
+@end
+
--- TestApp/TestApp/ViewController.m
+++ TestApp/TestApp/ViewController.m
@@ -6,8 +6,16 @@
 //
 
 #import "ViewController.h"
+#import <MapKit/MapKit.h>
 
-@interface ViewController ()
+#import "LocationManager.h"
+
+@interface ViewController () <MKMapViewDelegate>
+
+@property (nonatomic, weak) MKMapView *mapView;
+@property (nonatomic, strong) LocationManager *locationManager;
+//@property (nonatomic, weak) NSString *placemarkName;
+//@property (nonatomic, weak) NSString *placemarkCountry;
 
 @end
 
@@ -15,8 +23,127 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self.view setBackgroundColor:[UIColor redColor]]; 
+    
+    self.locationManager = [LocationManager new];
+    [self.locationManager start];
+    
+    self.title = @"Map Example";
+    self.view.backgroundColor = [UIColor yellowColor];
+    
+    //Карта
+    MKMapView *map = [[MKMapView alloc] initWithFrame:self.view.bounds];
+    self.mapView = map;
+    self.mapView.delegate = self;
+    
+    //Локация и регион
+//    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(self.locationManager.currentLocation.coordinate.latitude, self.locationManager.currentLocation.coordinate.longitude);
+//    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(coordinate, 1000000, 1000000);
+//    [self.mapView setRegion:region animated:YES];
+    
+    //Метка
+    
+
+    self.locationManager.locationCallback = ^(CLLocation *location) {
+    
+    [self addressFromLocation: location withCompletion:^(CLPlacemark *placemark) {
+//        self.placemarkName = placemark.name;
+//        self.placemarkCountry = placemark.country;
+        
+        MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(location.coordinate, 1000000, 1000000);
+        [self.mapView setRegion:region animated:YES];
+        
+        MKPointAnnotation *annotation = [MKPointAnnotation new];
+        annotation.title = placemark.name;
+        annotation.subtitle = placemark.country;
+        annotation.coordinate = location.coordinate;
+        
+        [self.mapView addAnnotation:annotation];
+        
+    }];
+    };
+    
+//    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(self.locationManager.currentLocation.coordinate.latitude, self.locationManager.currentLocation.coordinate.longitude);
+//    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(coordinate, 1000000, 1000000);
+//    [self.mapView setRegion:region animated:YES];
+//
+//    MKPointAnnotation *annotation = [MKPointAnnotation new];
+//    annotation.title = @"SuperTitle";
+//    annotation.subtitle = @"Subtitle";
+//    annotation.coordinate = coordinate;
+//
+//    [self.mapView addAnnotation:annotation];
+
+    [self.view addSubview:self.mapView];
+    
+    
+//    CLLocation *location = [[CLLocation alloc] initWithLatitude:55.752200 longitude:37.6155600];
+//    [self addressFromLocation:location];
+    
+    
+    
+//    [self locationFromAddress:@"Moscow, Nikolskay 23"];
+    
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [self.locationManager start];
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [self.locationManager stop];
+}
+
+#pragma mark - MKMapViewDelegate
+
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation {
+    
+    static NSString *identifier = @"AnnotationIdentifer";
+    MKAnnotationView *annotationView = [mapView dequeueReusableAnnotationViewWithIdentifier:identifier];
+    
+    if (annotationView) {
+        annotationView.annotation = annotation;
+    } else {
+        annotationView = [[MKMarkerAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:identifier];
+        annotationView.canShowCallout = YES;
+        annotationView.calloutOffset = CGPointMake(0.0, 5.0);
+        annotationView.rightCalloutAccessoryView = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+    }
+    
+    return annotationView;
+}
+
+#pragma mark - Private
+
+// Location -> Address
+- (void)addressFromLocation:(CLLocation *)location withCompletion:(void(^)(CLPlacemark *placemark))completion{
+    
+    CLGeocoder *geocoder = [CLGeocoder new];
+    [geocoder reverseGeocodeLocation:location completionHandler:
+     ^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
+        
+        if (placemarks.count > 0) {
+            for (MKPlacemark *placemark in placemarks) {
+                completion(placemark);
+            }
+        }
+    }];
+}
+
+// Address -> Location
+- (void)locationFromAddress:(NSString *)address {
+    CLGeocoder *geocoder = [CLGeocoder new];
+    
+    [geocoder geocodeAddressString:address completionHandler:
+        ^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
+            
+            if (placemarks.count > 0) {
+                for (MKPlacemark *placemark in placemarks) {
+                    NSLog(@"Placemark Location \n\n - %@", placemark.location);
+                }
+            }
+    }];
 }
 
 
 @end
+
